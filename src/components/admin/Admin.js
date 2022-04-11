f70a044a489009bbce8997e0dfff7d63a6586d99
baanwara papiha <baanwarapapiha@gmai.com>
2022-04-11 11:35:04 +0500

    start work on lagging and home

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -5,37 +5,38 @@ import FeatureList from ".//FeatureList";
 import PersonList from "./PersonList";
 import EvaluationList from "./EvaluationList";
 import SurveyList from "./SurveyList";
-import { Container, Grid, Paper } from '@mui/material';
+import { Container, Grid, Paper, Button, Stepper, Step, StepLabel } from '@mui/material';
 import { useState } from 'react';
 
 function Admin() {
   const [adminpage, setAadminpage] = useState(0)
+  const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+
   const pages = {0: '', 1:<SurveyForm />, 2: <PersonList/>, 3: <FeatureList/>}
   return (
     <Container>
+      {/* <br/>
+    <Stepper activeStep={1} alternativeLabel>
+      {steps.map((label) => (
+        <Step key={label}>
+          <StepLabel>{label}</StepLabel>
+        </Step>
+      ))}
+    </Stepper> */}
+
       <br/>
-      <Grid container spacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} alignItems="center" justify="center">
-        <br />
-        <Grid item xs={12} md={12}>
-          <Paper elevation={3}>
-          Create a new Survey
-          {pages[adminpage]}
-          <button onClick={()=>{setAadminpage(adminpage + 1);alert(adminpage)}}>
-            Start New Survey
-          </button>
-          </Paper>
-        </Grid>
+      <Paper elevation={3} style={{margin: "auto"}}>
+      <Button variant="outlined" onClick={()=>{setAadminpage(adminpage + 1);alert(adminpage)}}>
+        Start New Survey
+      </Button>
+      {pages[adminpage]}
 
-        {/* <SurveyForm /> */}
-        <SurveyList />
-        {/* <PersonForm /> */}
-        {/* <FeatureForm /> */}
-        {/* <FeatureList/> */}
-        {/* <PersonList/> */}
-        <EvaluationList/>
-        {/* <Tabl123 title={["name", "email"]}/> */}
+      </Paper>
+    {/* </Grid> */}
 
-      </Grid>
+    <SurveyList />
+    <EvaluationList/>
+
   </Container>
   );
 }
